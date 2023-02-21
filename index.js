c822eb35ff5c8c9620b50d830e886f79138e4171
valentina <godoyvalentina612@gmail.com>
2023-02-21 12:45:04 -0500

    mongodb

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const app = express()
-const port = 3035
+//const express = require("express")
+//const app = express()
+//const port = 3035
 
-app.get("/", (req,res) => {
-    res.send("hola mundo")
-}) 
+//app.get("/", (req,res) => {
+//    res.send("hola mundo")
+//}) 
 
-app.listen(port, () =>{
-    console.log ("Mi puerto es "+ port)
-})
+//app.listen(port, () =>{
+ //   console.log ("Mi puerto es "+ port)
+//n})
