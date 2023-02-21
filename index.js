9b635b714fd8ed743950cbde4b651c6cd7011573
valentina <godoyvalentina612@gmail.com>
2023-02-21 11:01:23 -0500

    Planeacion carpetas

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log ("Hola mundo")
+const express = require("express")
+const app = express()
+const port = 3035
+
+app.get("/", (req,res) => {
+    res.send("hola mundo")
+}) 
+
+app.listen(port, () =>{
+    console.log ("Mi puerto es "+ port)
+})
