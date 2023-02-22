edc7a9fbc5eaaf25b424eedf147eabce5d7aaaae
valentina <godoyvalentina612@gmail.com>
2023-02-22 16:11:31 -0500

    actualización

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -4,7 +4,7 @@ const RetsiController = require("../Controllers/appController")
 
 router.get("/character", RetsiController.getListado)
 router.post("/newCharacter", RetsiController.postNewListado)
-router.put("/Character/:id")
-router.delete("/RemoveCharacter")
+router.put("/Character/:id", RetsiController.putListado)
+router.delete("/RemoveCharacter", RetsiController.deleteListado)
 
 module.exports = router;
